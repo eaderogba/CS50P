2eab977f5339bd33eef2aef86f2c44612ffd2a95
eaderogba <eaderogba279@stu.ui.edu.ng>
2024-01-05 17:26:32 +0100

    added advanced meal.py and made modificatiions to meal.py

--- Week_1/meal_adv.py
+++ Week_1/meal_adv.py
@@ -1,28 +1,23 @@
 def main():
-    # Call the convert function, and store return value in time
-    # Prompt user for time
+    # Prompt user for time and pass input into the convert function: store value in time
     time = convert(input("What time is it? ").strip())
-    """breakfast between 7:00 and 8:00, lunch between 12:00 and 13:00, and dinner between 18:00 and 19:00"""
+    """breakfast between 7:00 and 8:00, lunch between 12:00 and 13:00 (12:00 p.m. and 1:00 p.m.), and dinner between 18:00 and 19:00 (6:00 p.m. and 7:00 p.m.)"""
     if 7 <= time <= 8:
-        print("breakfast time")
+        print("Breakfast time")
     elif 12 <= time <= 13:
-        print("lunch time")
+        print("Lunch time")
     elif 18 <= time <= 19:
-        print("dinner time")
-    else:
-        pass
+        print("Dinner time")
 
 # A function that converts time to the corresponding number of hours as a float
-
-
 def convert(time):
-    hours, minutes = map(float, time.replace(
+    hours, minutes = map(int, time.replace(
         "a.m.", "").replace("p.m.", "").split(":"))
     if "p.m." in time and hours != 12:
         hours += 12
     if "a.m." in time and hours == 12:
         hours = 0
-    return hours + (minutes / 60)
+    return hours + minutes / 60
 
 
 if __name__ == "__main__":
